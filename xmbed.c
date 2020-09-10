3af53443f113b1758fe44f92e3e88c5d651a2104
Chris Sauer <csauer@peoplenetonline.com>
2020-09-10 17:48:02 -0500

    use gtk_socket_add instead of passing the socket to a supported window

--- xmbed.c
+++ xmbed.c
@@ -6,6 +6,13 @@
 #include <gtk/gtkx.h>
 
 //gcc -o simple ./xmbed.c `pkg-config --libs --cflags gdk-3.0 gtk+-3.0` -lX11
+//https://stackoverflow.com/questions/38798252/whats-the-counterpart-of-qx11embedwidget-in-qt5
+//https://developer.gnome.org/gtk3/stable/GtkSocket.html
+
+static void my_gtk_realize(GtkWidget* widget, gpointer user)
+{
+    gtk_widget_set_window(widget, (GdkWindow*)user);
+}
 
 gint main(gint argc, gchar **argv)
 {
@@ -34,18 +41,52 @@ gint main(gint argc, gchar **argv)
 	
 	//Set default start size a little bigger. 
 	gtk_widget_set_size_request(sock, 400, 400);
-	gtk_box_pack_start(GTK_BOX(vbox), sock, TRUE,  TRUE, 0);
-	gtk_container_add(GTK_CONTAINER(win), vbox);
+	//gtk_box_pack_start(GTK_BOX(vbox), sock, TRUE,  TRUE, 0);
+	gtk_container_add(GTK_CONTAINER(win), sock);
 	//realize gtk window (required before embedding another window)
+	gtk_widget_show( sock);
+	gtk_widget_realize( sock );
 	gtk_widget_show_all(win);
+
+
 
 	/* Embed vim */
 	//get the id of the gtk socket
 	Window id = gtk_socket_get_id(GTK_SOCKET(sock));
-	//tell gvim to embed to this window
-	gchar *command = g_strdup_printf("gvim --servername %d --socketid %d", id, id);
-	g_spawn_command_line_async(command, NULL);
 
+	//tell gvim to embed to this window
+	//gchar *command = g_strdup_printf("gvim --socketid %d",  id);
+	//g_spawn_command_line_async(command, NULL);
+	//Obiously this is no fun because it means the app needs to get launched with socketid 
+	//but fret not gtk_socket_add_id is what the real app would use which works the other way
+
+
+	//x11 window
+	GdkDisplay* gd = gdk_display_get_default();
+    Display* d = GDK_DISPLAY_XDISPLAY(gd);
+    Window w = XCreateSimpleWindow(d, DefaultRootWindow(d), 0, 0, 300, 300, 0, 0, 0);
+
+	gtk_socket_add_id(GTK_SOCKET(sock),w);
+    
+    // Adding a gtk window to the X11 window because I want to interact with a button
+    GdkWindow* gw = gdk_x11_window_foreign_new_for_display(gd, w);
+    GtkWidget* gtk = gtk_widget_new(GTK_TYPE_WINDOW, NULL);
+    g_signal_connect(gtk, "realize", G_CALLBACK(my_gtk_realize), gw);
+    gtk_widget_set_has_window(gtk, TRUE);
+    gtk_widget_realize(gtk);
+
+    GtkWidget* menubar = gtk_menu_bar_new();
+    GtkWidget* file = gtk_menu_item_new_with_label("File");
+    GtkWidget* filemenu = gtk_menu_new();
+    GtkWidget* quit = gtk_menu_item_new_with_label("Quit");
+    gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quit);
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
+
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(box), menubar, FALSE, FALSE, 3);
+    gtk_container_add(GTK_CONTAINER(gtk), box);
+    gtk_widget_show_all(gtk);
 
 	/* Run */
 	gtk_main();
