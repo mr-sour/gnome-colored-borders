d9e2f625df3499803ee7ede3abb800899753a9df
Your Name <mr-sour@users.noreply.github.com>
2021-04-01 01:17:17 -0500

    extra ramblings

--- xmbed.c
+++ xmbed.c
@@ -9,16 +9,10 @@
 //https://stackoverflow.com/questions/38798252/whats-the-counterpart-of-qx11embedwidget-in-qt5
 //https://developer.gnome.org/gtk3/stable/GtkSocket.html
 
-static void my_gtk_realize(GtkWidget* widget, gpointer user)
-{
-    gtk_widget_set_window(widget, (GdkWindow*)user);
-}
-
-
-static void reparent_x11win_gtk_csd(Window w)
+static void x11_win_reparent_to_gtk_csd(Window w)
 {
 
-	/* Create window */
+	/* Create window Objects*/
 	GtkWidget *win  = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	GtkWidget *vbox = gtk_box_new(FALSE, 0);
 	GtkWidget *sock = gtk_socket_new();
@@ -26,30 +20,40 @@ static void reparent_x11win_gtk_csd(Window w)
 	GdkDisplay *display = gdk_display_get_default();
 	GdkScreen *screen = gdk_display_get_default_screen (display);
 	GtkCssProvider *provider = gtk_css_provider_new ();
-	//Add provider to style headerbar
-
+	
+	//Add provider to style headerbar priority is 65535 which means it can't be overriden 
 	gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER(provider), 65535);
-        gtk_css_provider_load_from_data (
+    //Simple little decoration to add to the window
+    gtk_css_provider_load_from_data (
         provider, " decoration { border: 3px solid green; background: gray; }           	.titlebar { background: green ; color:white; }           	.titlebar:backdrop  {background: green; color:white;}           	window.ssd headerbar.titlebar { border: 5px; box-shadow: none;          	background-image: linear-gradient(to bottom, shade(blue, 1.05),           	shade(blue, 1.00)); }", -1, NULL);
-        //swap titlebar for headerbar to set CSD
+    //I like windows that close
 	gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (header), TRUE);
-        gtk_header_bar_set_has_subtitle (GTK_HEADER_BAR (header), FALSE);
-        gtk_header_bar_set_title (GTK_HEADER_BAR (header), "x11 window embedded in a window with CSD");
+	//subtitles? this show is already in my language
+    gtk_header_bar_set_has_subtitle (GTK_HEADER_BAR (header), FALSE);
+    //this would get set to "VMNAME - APPLICTION NAME" in the real app
+    gtk_header_bar_set_title (GTK_HEADER_BAR (header), "x11 window embedded in a window with CSD");
+	//swap default titlebar for headerbar to set CSD
 	gtk_window_set_titlebar (GTK_WINDOW (win), header);
-	//connect socket
+	
+	//connect sockets
 	g_signal_connect(sock, "plug-removed", gtk_main_quit, NULL);
 	g_signal_connect(win,  "delete-event", gtk_main_quit, NULL);
 
-	//Set default start size a little bigger. 
+	//Set default start size a little bigger.IDK might need todo some weirdness in the real but this is fine for demo 
 	gtk_widget_set_size_request(sock, 400, 200);
+	//why did add this?
 	//gtk_box_pack_start(GTK_BOX(vbox), sock, TRUE,  TRUE, 0);
+
+	//gotta add my wonderful nonexistant widgets or the sockets dont work
 	gtk_container_add(GTK_CONTAINER(win), sock);
+	
 	//realize gtk window (required before embedding another window)
-	gtk_widget_show( sock);
+	gtk_widget_show(sock);
 	gtk_widget_realize( sock );
 	gtk_widget_show_all(win);
     // embed x11 window into a GtkSocket
     gtk_socket_add_id(GTK_SOCKET(sock),w);
+    //Call main cause it was in the example
 	gtk_main();
 
 }
@@ -58,36 +62,17 @@ static void reparent_x11win_gtk_csd(Window w)
 gint main(gint argc, gchar **argv)
 {
 
-
+    // the only reason this is here and not in the reparenting functin is that I need the display
+    //in the real app thats pulled from some x11 voodoo struct
 	gtk_init(NULL, NULL);
-
-
-	/* Embed vim */
-	//get the id of the gtk socket
-	
-	//Window id = gtk_socket_get_id(GTK_SOCKET(sock));
-
-	//tell gvim to embed to this window
-	//gchar *command = g_strdup_printf("gvim --socketid %d",  id);
-	//g_spawn_command_line_async(command, NULL);
-	//Obiously this is no fun because it means the app needs to get launched with socketid 
-	//but fret not gtk_socket_add_id is what the real app would use which works the other way
-
-
-	//x11 window
 	GdkDisplay* gd = gdk_display_get_default();
 
+    //get a display
     Display* d = GDK_DISPLAY_XDISPLAY(gd);
+    //the simplest black window you ever saw
     Window w = XCreateSimpleWindow(d, DefaultRootWindow(d), 0, 0, 300, 300, 0, 0, 0);
-    
-    reparent_x11win_gtk_csd(w);
-
-
-
-
-
-
-
+    //magic reparenting function 
+    x11_win_reparent_to_gtk_csd(w);
 
 	/* Run */
 	return 0;
